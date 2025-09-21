5f6b2497120b8423d1e5c9a500d50ea0a1632656
booth0 <10toasterv2@gmail.com>
2025-09-20 23:40:37 -0600

    fix

--- server.js
+++ server.js
@@ -2,16 +2,6 @@ import express from 'express';
 import homeRoutes from './routes/home.js';
  
 const app = express();
-<<<<<<< HEAD
-=======
-// const name = 'Scarlet Booth';
-const name = process.env.NAME || 'Scarlet Booth';
- 
-app.get('/', (req, res) => {
-    res.send(`${name}`);
-});
- 
->>>>>>> a3c4e35de0154e58c264c58da71393d1ba39dd28
 const PORT = 3000;
 
 app.use(express.json());
